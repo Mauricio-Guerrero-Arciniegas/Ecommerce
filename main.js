a2df5aec6ca6b4e03eb8bb00fd18b2afcd81ed10
Mauricio-Guerrero-Arciniegas <maurizioguerrero@hotmail.com>
2022-04-22 18:22:55 -0500

    cart

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 // Cart
-let cartIcon = document.querySelector("#cart-icon");
+let cartIcon = document.querySelector('#cart-icon');
 let cart = document.querySelector(".cart");
 let closeCart = document.querySelector("#close-cart");
 // Open Cart
-cartIcon.onclick = () => {
+cartIcon.onclick = () =>{
     cart.classList.add("active");
 };
 // Close Cart
-closeCart.onclick = () => {
-    cart.classList.remove();
+closeCart.onclick = () =>{
+    cart.classList.remove("active");
 };
+
